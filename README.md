4003f6a27f55e7be3d7ec2c3581a1c939e3c3228
Lee Jones <prolific.jones82@gmail.com>
2012-10-25 23:50:59 +0100

    Collections API upload

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ A bootleg Redbubble API.
 
 API v1 -- Pulls all the preview image and link to the product from the first page of a profiles 'Shop' tab.
 
+API Collections -- Pulls a users collections and images inside the collections.
+
 I'm going to be working on a full API over the coming weeks.
---
+++ redbubble-api-collections.php
@@ -0,0 +1,128 @@
+<?php
+
+/*   Redbubble API - Collections
+ *   Written by Lee Jones (mail@leejones.me.uk)
+ *   Project Home Page: https://github.com/prolificjones82/Redbubble-API
+ *   Released under GNU Lesser General Public License (http://www.gnu.org/copyleft/lgpl.html)
+ *
+ *
+ *	 Please submit all problems or questions to the Issues page on the projects GitHub page:
+ *   https://github.com/prolificjones82/Redbubble-API
+ *
+ */ 
+
+$rburl = "http://www.redbubble.com";
+
+// ERROR LIST
+$error1 = "The user <i><b>'" . $_GET['rb_user'] . "'</b></i> doesn't exist on Redbubble.<br />\n"
+	. "<a href='" . $_SERVER['HTTP_REFERER'] . "'><< Go Back</a>";
+
+if (!empty($_GET['rb_user']) && empty($_GET['coll'])) {
+	$url = sprintf($rburl . "/people/%s/portfolio/", $_GET['rb_user']);
+
+	if ($xhtml = @file_get_contents($url, FILE_SKIP_EMPTY_LINES)) {
+		$data = array();
+		
+		$doc = new DOMDocument();
+			if (!@$doc->loadHTML($xhtml)) {
+				die($error2);
+			}
+		
+		// Grab Link URL
+		$xpath = new DOMXpath($doc);
+		$itemquery = "//a[contains(concat(' ',normalize-space(@class),' '), 'collection-link')]";
+		$items = $xpath->query($itemquery);
+		
+		echo "<h1>Collections</h1>";
+		
+		echo "<ul>";
+		foreach($items as $item) {
+			echo "<a href='" . $_SERVER['REQUEST_URI'] . "&coll=";
+			
+			// Strip link to leave collection ID and title
+			$pattern = "/people/" . $_GET['rb_user'] . "/collections/";
+			$coll_id = str_replace($pattern, "", $item->getAttribute('href'));
+			echo $coll_id;
+			
+			echo "''><li>";
+			
+			// Grab Background Image of collection link
+			$strip = array('background-image: url(', ');');
+			$img = $item->getAttribute('style');
+			$bgimg = str_replace($strip, '', $img);
+			echo "<img src='" . $bgimg . "' border='1' />";
+			
+			// Grab Collection Title
+			if ($title = $item->getElementsByTagName('span')) {
+				$data['title'] = $title->item(0)->nodeValue;
+			}
+			echo "<h5>" . $data['title'] . "</h5>";
+			
+			
+			echo "</li></a>";
+		}
+		echo "</ul>";
+	
+	} else {
+		die($error1);
+	}
+
+
+} elseif (!empty($_GET['coll'])) {	
+
+	$url = sprintf($rburl . "/people/%s/collections/%s", $_GET['rb_user'], $_GET['coll']);
+
+	if ($xhtml = @file_get_contents($url, FILE_SKIP_EMPTY_LINES)) {
+		$data = array();
+		
+		$doc = new DOMDocument();
+			if (!@$doc->loadHTML($xhtml)) {
+				die($error2);
+			}
+		
+		// Grab Link URL
+		$xpath = new DOMXpath($doc);
+		$itemquery = "//a[contains(concat(' ',normalize-space(@class),' '), 'shop-product')]";
+		$items = $xpath->query($itemquery);
+		
+		// Back Link
+		echo "<a href='" . $_SERVER['HTTP_REFERER'] . "'>&laquo; Back</a>";
+		
+		echo "<ul>";
+		foreach($items as $item) {
+			echo "<a href='" . $rburl . $item->getAttribute('href') . "' target='_blank'><li>";
+			
+			// Get Images
+			if ($img = $item->getElementsByTagName('img')) {
+				$data['img'] = $img->item(0)->getAttribute('src');
+			}
+			echo "<img src='" . $data['img'] . "' />";
+			
+			if ($prices = $item->getElementsByTagName('span')) {
+				$data['price'] = $prices->item(2)->nodeValue;
+			}
+			echo "<p><b>" . substr($data['price'], 1) . "</b></p>";
+			
+			echo "</li></a>";
+		}
+		echo "</ul>";
+		
+		// Back Link
+		echo "<a href='" . $_SERVER['HTTP_REFERER'] . "'>&laquo; Back</a>";
+	
+	} else {
+		die($error1);
+	}
+	
+} else {
+	$form = "<form method='get'>\n"
+		. "<table>\n"
+		. "<tr><th>Redbubble Username:</th><td><input type='text' name='rb_user' /></td></tr>\n"
+		. "<tr><th>&nbsp;</th><td><input type='submit' value='Go' /></td></tr>\n"
+		. "</table>\n"
+		. "</form>";
+		
+	print($form);
+}
+
+?>
